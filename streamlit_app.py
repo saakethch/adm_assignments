862a8047f90f9245fad9d6a8d8031f5951b53ba7
saakethch <123130490+saakethch@users.noreply.github.com>
2023-04-15 02:01:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,34 +2,17 @@ import streamlit as st
 import pinecone
 import openai
 
-st.title("ADM Assignment 5 - ")
-index_name = 'openai-youtube-mac-transcriptions-a5'
-
-# initialize connection to pinecone (get API key at app.pinecone.io)
-pinecone.init(
-    api_key="cd5700c8-f94e-4688-94ee-20c4c451d36b",
-    environment="northamerica-northeast1-gcp"
-)
-
-# check if index already exists (it shouldn't if this is first time)
-if index_name not in pinecone.list_indexes():
-    # if does not exist, create index
-    pinecone.create_index(
-        index_name,
-        dimension=len(res['data'][0]['embedding']),
-        metric='cosine',
-        metadata_config={'indexed': ['channel_id', 'published']}
-    )
-
-# connect to index
-index = pinecone.Index(index_name)
+st.title("ADM Assignment 5 - GPT Based RAG KG ")
 
 limit = 3750
 openai.api_key = st.secrets["OPENAI_API_KEY"]
+embed_model = "text-embedding-ada-002"
+
+
 def retrieve(query):
     res = openai.Embedding.create(
         input=[query],
-        engine="text-embedding-ada-002"
+        engine=embed_model
     )
 
     # retrieve from Pinecone
@@ -49,25 +32,10 @@ def retrieve(query):
     prompt_end = (
         f"\n\nQuestion: {query}\nAnswer:"
     )
-    # append contexts until hitting limit
-    for i in range(1, len(contexts)):
-        if len("\n\n---\n\n".join(contexts[:i])) >= limit:
-            prompt = (
-                prompt_start +
-                "\n\n---\n\n".join(contexts[:i-1]) +
-                prompt_end
-            )
-            break
-        elif i == len(contexts)-1:
-            prompt = (
-                prompt_start +
-                "\n\n---\n\n".join(contexts) +
-                prompt_end
-            )
+    prompt = prompt_start + contexts[0] + prompt_end
     return prompt
 
 def complete(prompt):
-    # query text-davinci-003
     res = openai.Completion.create(
         engine='text-davinci-002',
         prompt=prompt,
@@ -80,13 +48,36 @@ def complete(prompt):
     )
     return res['choices'][0]['text'].strip()
 
+index_name = 'openaiyoutube'
+
+# initialize connection to pinecone (get API key at app.pinecone.io)
+pinecone.init(
+    api_key=st.secrets["PINECONE_API_KEY"], #
+    environment="us-west4-gcp"
+)
+
+# check if index already exists (it shouldn't if this is first time)
+if index_name not in pinecone.list_indexes():
+    # if does not exist, create index
+    pinecone.create_index(
+        index_name,
+        dimension=len(res['data'][0]['embedding']),
+        metric='cosine',
+        metadata_config={'indexed': ['channel_id', 'published']}
+    )
+
+# connect to index
+index = pinecone.Index(index_name)
 def ask_me(prompt):
     query = prompt
     query_with_contexts = retrieve(query)
+    print(query_with_contexts)
     return complete(query_with_contexts)
 
-# user input
-apparel_prompt = st.text_input("Enter a question related to Canon EOS R5 : ")
 st.image("images.jpeg")
-ans = ask_me(apparel_prompt)
-st.write(ans)
+
+# user input
+user_prompt = st.text_input("Enter a question related to Canon R5: ")
+if len(user_prompt)>5:
+    ans = ask_me(user_prompt)
+    st.write(ans)
